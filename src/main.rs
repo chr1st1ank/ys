fe85d3ee2b293457761c31a58fcbfbb171d3881d
chr1st1ank <chr1st1ank@krudewig-online.de>
2022-08-14 13:58:59 +0200

    Implement printing one file's contents to stdout.
    Closes #3 (Read from one file) and closes #2 (Write to stdout)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    let file_handle = File::open(file_path);
+    if let Err(e) = file_handle {
+        eprintln!("Error opening file. {}", e);
+        process::exit(1);
+    }
+    let reader = BufReader::new(file_handle.unwrap());
+    for line in reader.lines() {
+        println!("{}", line.unwrap());
+    }
 }
