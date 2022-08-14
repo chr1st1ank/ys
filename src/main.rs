bfcfc92f0e93b5d6d89ecbdb022f2008ada14e1d
chr1st1ank <chr1st1ank@krudewig-online.de>
2022-08-15 01:28:26 +0200

    PoC for syntax highlighting

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, Error};
 use std::process;
+use syntect::{easy, highlighting};
 
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
@@ -27,7 +28,7 @@ fn error_exit(e: Error) {
 
 fn process_stream(file_path: Option<String>) -> Result<(), io::Error> {
     let reader = open_input_stream(file_path)?;
-    write_out(reader)?;
+    write_out(reader, true)?;
     Ok(())
 }
 
@@ -41,9 +42,36 @@ fn open_input_stream(file_path: Option<String>) -> Result<Box<dyn io::BufRead>, 
     }
 }
 
-fn write_out(stream: Box<dyn io::BufRead>) -> Result<(), io::Error> {
-    for line in stream.lines() {
-        println!("{}", line?);
+fn write_out(stream: Box<dyn io::BufRead>, formatted: bool) -> Result<(), io::Error> {
+    if formatted {
+        let syntax_set = syntect::parsing::SyntaxSet::load_defaults_nonewlines();
+        let theme_set = highlighting::ThemeSet::load_defaults();
+        let mut highlighter = easy::HighlightLines::new(
+            syntax_set.find_syntax_by_extension("yml").unwrap(),
+            &theme_set.themes["Solarized (dark)"],
+        );
+
+        for line in stream.lines() {
+            let str_line = line?;
+            let escaped = highlight_for_shell(&mut highlighter, &syntax_set, str_line);
+            println!("{}", escaped);
+        }
+    } else {
+        for line in stream.lines() {
+            println!("{}", line?);
+        }
     }
     Ok(())
+}
+
+fn highlight_for_shell(
+    highlighter: &mut easy::HighlightLines,
+    syntax_set: &syntect::parsing::SyntaxSet,
+    str_line: String,
+) -> String {
+    let ranges: Vec<(highlighting::Style, &str)> = highlighter
+        .highlight_line(str_line.as_str(), syntax_set)
+        .unwrap();
+    let escaped = syntect::util::as_24_bit_terminal_escaped(&ranges[..], true);
+    escaped
 }
